ced6a59b6a5d0618d19e8235f691bffbd8c5b455
Tejeswara Sai Appikatla <appikatlahome@gmail.com>
2021-01-05 16:38:10 +0530

    Variable Declaration and Initialization Approach

---
+++ FSD Course 1/src/javaBasics/VariableAccessor.java
@@ -0,0 +1,13 @@
+package javaBasics;
+
+public class VariableAccessor 
+{
+	public static void main(String[] args)
+	{
+		Variables object = new Variables();
+		object.method();
+		System.out.println(object.data);
+		
+	}
+
+}
--- FSD Course 1/src/javaBasics/Variables.java
+++ FSD Course 1/src/javaBasics/Variables.java
@@ -2,6 +2,12 @@ package javaBasics;
 
 public class Variables 
 {
+	public int data = 50;		//Instance Variable
+	public static int m = 100;	//Static Variable
 	
+	void method()
+	{
+		int n = 90;				// Local Variable
+	}
 
 }
