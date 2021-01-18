0270ab405790c790e3fadf56d86db6d0f9dab9d7
Tejeswara Sai Appikatla <appikatlahome@gmail.com>
2021-01-18 16:34:56 +0530

    Getters and Setters Added as User.java as Getter and Setter Class where GetSet.java acts as a main method for accessing User.java Class

---
+++ FSD Course 1/src/javaBasics/GetSet.java
@@ -0,0 +1,14 @@
+package javaBasics;
+
+public class GetSet 
+{
+	public static void main(String[] args)
+	{
+		User obj = new User();
+		obj.setUserId(100);
+		System.out.println(obj.getUserId());
+		
+	}
+	
+
+}
---
+++ FSD Course 1/src/javaBasics/User.java
@@ -0,0 +1,16 @@
+package javaBasics;
+
+public class User 
+{
+	int userId = 50;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+
+}
